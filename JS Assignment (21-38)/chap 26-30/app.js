d37638f8d6bf20d5e78877af837cb70bb09ea467
FareenaEhtesham <57524242+FareenaEhtesham@users.noreply.github.com>
2020-06-21 10:53:54 -0400

    Chapter 26-30

---
+++ JS Assignment (21-38)/chap 26-30/app.js
@@ -0,0 +1,62 @@
+//CHAPTER 26-30
+//TASK 1
+// var no = prompt("Enter a positive integer:");
+// alert("Number :"+ " "+ no);
+// var round= Math.round(no);
+// var floor = Math.floor(no);
+// var ceiling = Math.ceil(no);
+// alert("Round off value: " + " " + round);
+// alert("Floor value: " + " " + floor);
+// alert("Ceil value : " + " " + ceiling);
+
+//TASK 2
+// var no = prompt("Enter a negative integer:");
+// alert("Number :"+ " "+ no);
+// var round= Math.round(no);
+// var floor = Math.floor(no);
+// var ceiling = Math.ceil(no);
+// alert("Round off value: " + " " + round);
+// alert("Floor value: " + " " + floor);
+// alert("Ceil value : " + " " + ceiling);
+
+//TASK 3
+// var no = prompt("Enter a number:");
+// var absol = Math.abs(no);
+// alert(" The absolute value of " + " " + no +" " + "is " + " " +absol);
+
+//TASK 4
+// var dice = Math.random()*6;
+// var ceil = Math.ceil(dice);
+// alert("Random dice value: " + " " + ceil);
+
+//TASK 5
+// var toss = Math.random()*2;
+// var ans = Math.ceil(toss);
+// if(ans===2)
+// {
+//     alert("Random coin value: Heads");
+// }
+// if(ans===1)
+// {
+//     alert("Random coin value: Tails");
+// }
+
+//TASK 6
+// var random = Math.random()*100;
+// var numberr = Math.ceil(random);
+// alert("Random number between 1 and 100: " + " " + numberr);
+
+//TASK 7
+// var kg = prompt("Enter your weight in kilograms:");
+// alert("The weight of user is: " + " " + kg + " " + "kilograms");
+
+//TASK 8
+// var secret = Math.random()*10;
+// var secretno = Math.ceil(secret);
+// var numb = prompt ("Enter a number between 1 and 10:");
+// if (numb == secretno){
+//     alert("Congratulations! You guessed it right! ")
+// }
+// else{
+//     alert("Try Again!");
+// }
